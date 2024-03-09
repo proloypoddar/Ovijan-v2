692f469de80929743a519ee57902254c86981184
proloypoddar <iamproloy9@gmail.com>
2024-03-09 09:25:29 +0600

    update

--- Website/script.js
+++ Website/script.js
@@ -1,11 +1,8 @@
-// script.js
 document.addEventListener("DOMContentLoaded", function() {
-    // Function to generate random number within a range
     function generateRandomNumber(min, max) {
         return Math.random() * (max - min) + min;
     }
 
-    // Function to update the environment parameters display
     function updateEnvironmentParametersDisplay() {
         var temperatureElement = document.getElementById("temperature");
         var co2Element = document.getElementById("co2");
@@ -18,15 +15,15 @@ document.addEventListener("DOMContentLoaded", function() {
         var aqiElement = document.getElementById("aqi");
 
         var temperature = Math.floor(generateRandomNumber(25, 27));
-        var co2Level = 421; // Set CO2 level to be around 421 ppm
+        var co2Level = 421;
         var humidity = Math.floor(generateRandomNumber(64, 68));
-        var calibrate = Math.random() < 0.5 ? "True" : "False"; // Randomly set to True or False
+        var calibrate = Math.random() < 0.5 ? "True" : "False";
         var o3PPM = generateRandomNumber(77409240, 83561712).toFixed(2);
         var methane = generateRandomNumber(-42.15, -40.47).toFixed(2);
         var alcohol = generateRandomNumber(-59.90, -60.26).toFixed(2);
         var propane = generateRandomNumber(-80.55, -82.21).toFixed(2);
-        var aqi = 20; // AQI value
-        var aqiStatus = "Good air"; // AQI status
+        var aqi = 20;
+        var aqiStatus = "Good air";
 
         temperatureElement.textContent = "Temperature: " + temperature + "°C";
         co2Element.textContent = "CO2 Level: " + co2Level + " ppm";
@@ -38,7 +35,6 @@ document.addEventListener("DOMContentLoaded", function() {
         propaneElement.textContent = "Propane: " + propane;
         aqiElement.textContent = "Air Quality Index (AQI): " + aqi.toFixed(2) + "\n" + aqiStatus;
 
-        // Check AQI status and update color accordingly
         if (aqi <= 50) {
             aqiElement.style.color = "green";
         } else if (aqi <= 100) {
@@ -52,30 +48,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    // Function to show about section
-    function showAboutSection() {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                var aboutSection = document.getElementById("aboutSection");
-                aboutSection.style.display = "block";
-                aboutSection.innerHTML = xhr.responseText;
-            }
-        };
-        xhr.open("GET", "about.txt", true);
-        xhr.send();
-    }
-
-    // Initial update of environment parameters display
     updateEnvironmentParametersDisplay();
 
-    // Update environment parameters display every 10 seconds
     setInterval(updateEnvironmentParametersDisplay, 10000);
-
-    // Event listener for the "About" button
-    var aboutButton = document.getElementById("aboutButton");
-    aboutButton.addEventListener("click", function(event) {
-        event.preventDefault(); // Prevent default behavior of anchor tag
-        showAboutSection();
-    });
 });
